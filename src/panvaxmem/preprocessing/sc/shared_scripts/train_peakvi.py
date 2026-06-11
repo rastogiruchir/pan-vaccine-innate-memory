c71f30f119af25aa42b41a28a815d6668bfd1574
Ruchir Rastogi <ruchir_rastogi@cs.berkeley.edu>
2026-06-11 13:35:09 -0700

    Add a script to train peakVI models

---
+++ src/panvaxmem/preprocessing/sc/shared_scripts/train_peakvi.py
@@ -0,0 +1,47 @@
+"""Train a peakVI model."""
+
+import os
+from argparse import ArgumentParser
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+
+import scanpy as sc
+import scvi
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--input-h5ad", required=True)
+    parser.add_argument("--output-h5ad", required=True)
+    parser.add_argument("--model-dir", required=True)
+    parser.add_argument("--history-csv", required=True)
+    parser.add_argument("--batch-key", default="donor")
+    parser.add_argument("--min-frac-cells", type=float, default=0.01)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    adata = sc.read_h5ad(args.input_h5ad)
+    adata.obs[args.batch_key] = adata.obs[args.batch_key].astype(str)
+    sc.pp.filter_genes(adata, min_cells=int(args.min_frac_cells * adata.shape[0]))
+    scvi.model.PEAKVI.setup_anndata(adata, batch_key=args.batch_key)
+    print(f"Training model with input data shape: {adata.shape}")
+
+    model = scvi.model.PEAKVI(adata)
+    model.train(early_stopping=True, check_val_every_n_epoch=1)
+    model.save(args.model_dir)
+
+    history = model.history["elbo_train"]
+    history["elbo_validation"] = model.history["elbo_validation"]
+    history.to_csv(args.history_csv)
+
+    adata.obsm["X_pvi"] = model.get_latent_representation()
+    sc.pp.neighbors(adata, use_rep="X_pvi")
+    sc.tl.umap(adata, min_dist=0.2)
+    adata.write(args.output_h5ad, compression="gzip")
+
+
+if __name__ == "__main__":
+    main()
